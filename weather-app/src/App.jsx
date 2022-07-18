6c7111a9dcec9054629c3415355d5b3007e34233
Damien <damienroot@icloud.com>
2022-07-18 19:12:28 +0100

    create SearchBar

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,7 +1,10 @@
+import SearchBar from "./components/SearchBar";
+
 export default function App() {
   return (
     <div className="App">
-      <p></p>
+      <h2 className="header__hero">Simple Weather</h2>
+      <SearchBar />
     </div>
   );
 }
---
+++ weather-app/src/components/SearchBar.jsx
@@ -0,0 +1,63 @@
+import { useState, useCallback } from "react";
+import Card from "./Card";
+
+export default function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [apiResults, setApiResults] = useState({
+    noNewSearchTerm: true,
+  });
+
+  const fetchData = useCallback(() => {
+    fetch(
+      `http://api.weatherapi.com/v1/current.json?key=54bb3ee2ef714beaa14124525212910&q=${searchTerm}&aqi=no`
+    )
+      .then((response) => response.json())
+      .then((data) => setApiResults(data));
+  }, [searchTerm]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchData();
+    setSearchTerm("");
+  };
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const a = () => {
+    if (apiResults.noNewSearchTerm === true) {
+      return (
+        <div>
+          <p>Enter a city</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Card apiResults={apiResults} />
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleChange}
+          value={searchTerm || ""}
+          type="text"
+          placeholder="Search for a city..."
+          name="search"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      {a()}
+    </div>
+  );
+}
+
+// 54bb3ee2ef714beaa14124525212910 - api key
+// http://api.weatherapi.com/v1/current.json?key=54bb3ee2ef714beaa14124525212910 &q=London&aqi=no
+// ^ api call url
