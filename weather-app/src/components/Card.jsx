f1d989c702334c52aaf7b246997ed2afaf787672
Damien <damienroot@icloud.com>
2022-07-18 19:12:04 +0100

    update Card

--- weather-app/src/components/Card.jsx
+++ weather-app/src/components/Card.jsx
@@ -1,11 +1,18 @@
 export default function Card({ apiResults }) {
+  const { current, location } = apiResults;
+  // api returns two objects
+
   return (
     <div>
-      <p>Temperature: </p>
-      <p>Forecast: </p>
-      <p>Humidity: </p>
-      <p>Pollution: </p>
-      <p>Pollen: </p>
+      <p>
+        {location.name}, {location.country}
+      </p>
+      <p>
+        Temperature: {current.temp_c}&#8451; (feels like {current.feelslike_c}
+        &#8451;)
+      </p>
+      <p>Forecast: {current.condition.text}</p>
+      <p>Humidity: {current.humidity}</p>
     </div>
   );
 }
