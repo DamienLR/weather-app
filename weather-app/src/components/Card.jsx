db769fac3b0453a904ee1643b374e22204912ad0
Damien <damienroot@icloud.com>
2022-07-18 14:21:08 +0100

    create Card

---
+++ weather-app/src/components/Card.jsx
@@ -0,0 +1,11 @@
+export default function Card({ apiResults }) {
+  return (
+    <div>
+      <p>Temperature: </p>
+      <p>Forecast: </p>
+      <p>Humidity: </p>
+      <p>Pollution: </p>
+      <p>Pollen: </p>
+    </div>
+  );
+}
