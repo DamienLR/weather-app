7ed60127836ab2026de86f4d76146ffc5e8d4064
Damien <damienroot@icloud.com>
2022-07-19 15:51:34 +0100

    style Card

--- weather-app/src/components/Card.jsx
+++ weather-app/src/components/Card.jsx
@@ -1,18 +1,20 @@
+import "../css/Card.css";
+
 export default function Card({ apiResults }) {
   const { current, location } = apiResults;
   // api returns two objects
 
   return (
-    <div>
-      <p>
+    <div className="card">
+      <p className="paragraph">
         {location.name}, {location.country}
       </p>
-      <p>
-        Temperature: {current.temp_c}&#8451; (feels like {current.feelslike_c}
-        &#8451;)
+      <p className="paragraph">
+        Temperature: {current.temp_c}&#8451; - feels like {current.feelslike_c}
+        &#8451;
       </p>
-      <p>Forecast: {current.condition.text}</p>
-      <p>Humidity: {current.humidity}</p>
+      <p className="paragraph">Forecast: {current.condition.text}</p>
+      <p className="paragraph">Humidity: {current.humidity}</p>
     </div>
   );
 }
